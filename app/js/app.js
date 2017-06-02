b2954c6e5f8f38df7e8f6cf3be2267ffdc95ec56
Nicolas C. Barbosa <nicolascb.claudio@gmail.com>
2017-06-02 00:52:13 -0300

    adjustments images + audios source examples and events

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,11 @@
 const countTimer = () => {
-    const doc = document;
-    const $audio = doc.querySelector('.js-audio-player');
+  const doc = document;
+  const $audio = doc.querySelector('.js-audio-player');
+  const $progress = doc.querySelector('.js-audio-progress');
+  const $timerFinish = doc.querySelector('.js-timer-finish');
+  const $timerInit = doc.querySelector('.js-timer-init');
 
   $audio.addEventListener('loadedmetadata', () => {
-    let $timerFinish = doc.querySelector('.js-timer-finish');
     let duration = parseInt($audio.duration);
     let secondsDuration = parseInt(duration % 60);
     let minutesDuration = parseInt((duration / 60) % 60);
@@ -13,10 +15,10 @@ const countTimer = () => {
   }, false);
 
   $audio.addEventListener('timeupdate', () => {
-    let $timerInit = doc.querySelector('.js-timer-init');
     let duration = parseInt($audio.duration);
     let currentTime = parseInt($audio.currentTime);
     let timeLeft = duration - currentTime;
+    let progress = (100 / duration) * currentTime;
     let seconds = timeLeft % 60;
     let minutes = Math.floor(timeLeft / 60) % 60;
 
@@ -24,6 +26,20 @@ const countTimer = () => {
     minutes = minutes < 10 ? "0"+minutes : minutes;
 
     $timerInit.textContent = minutes+":"+seconds;
+    $progress.value = progress;
+
+  }, false);
+
+
+  $progress.addEventListener('click', (e) => {
+    let selectTime = (e.offsetX * $progress.max / $progress.offsetWidth);
+    let duration = parseInt($audio.duration);
+
+    $progress.value = selectTime;
+    console.log(selectTime);
+    console.log(duration);
+    // $audio.currentTime = selectTime * duration;
+    console.log(selectTime);
   }, false);
 
 };
@@ -37,9 +53,28 @@ const controlVolume = () => {
 
   $control.addEventListener('change', () => {
     $audio.volume = $control.value;
-
-    console.log($audio.volume);
   }, false);
+
 };
 
 controlVolume.call();
+
+const customControls = () => {
+  const doc = document;
+  const $audio = doc.querySelector('.js-audio-player');
+  const $playPause = doc.querySelector('.js.-play-and-pause');
+  const $next = doc.querySelector('.js.-next-track');
+  const $prev = doc.querySelector('.js.-prev-track');
+
+  $playPause.addEventListener('click', () => {
+
+    if($audio.paused) {
+      $audio.play();
+    } else {
+      $audio.pause();
+    }
+
+  }, false);
+};
+
+customControls.call();
